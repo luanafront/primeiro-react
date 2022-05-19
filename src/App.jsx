78fba4b950c0c9c065421ec844aae4a7b1d601d7
luanafront <luana.lopes.rodrigues1@gmail.com>
2022-05-18 21:49:05 -0300

    coloquei botoes, imagens e titulos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import './App.css';
 import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
+import imagemReact from "./react.png"
 
 function App() {
   const [contador, incrementarContador] = useState(0)
@@ -13,14 +16,15 @@ function App() {
 
   return (
     <div className="primeiro__site--react">
-      <h1>Olá Mundo!</h1>
-      <h2>Este é meu primeiro site usando React</h2>
+      <img alt="react" className='imagem' src={imagemReact}/>
+      <h1 className="titulo">Olá Mundo! &nbsp;{<VolunteerActivismIcon/>}</h1>
+      <h2 className="subtitulo">Este é meu primeiro site usando React</h2>
 
-      <p>{contador}</p>
-      <button onClick={aumentar}>Aumentar</button>
-      <button onClick={diminuir}>Diminuir</button>
-
-
+      <p className="numeracao">{contador}</p>
+      <div className='botoes'>
+        <Button onClick={aumentar} variant="contained" color="secondary"  size="small">Aumentar</Button>
+        <Button onClick={diminuir} variant="contained" color="secondary"  size="small">Diminuir</Button>
+      </div>
     </div>
   );
 }
