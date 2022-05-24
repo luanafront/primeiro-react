dab37ab09e6372f4a5d52dc653b97d9fb18b8389
luanafront <luana.lopes.rodrigues1@gmail.com>
2022-05-24 14:08:41 -0300

    coloquei uma nova imagem

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Button from '@mui/material/Button';
 import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
 import imagemReact from "./react.png"
 import imagemGithub from "./github.png"
-import Luana from "./componentes/luana"
-import Churros from "./componentes/churros"
+import imagemEstudos from "./leitura.png"
+
 
 function App() {
   const [contador, incrementarContador] = useState(0)
@@ -28,21 +28,15 @@ function App() {
       <h1 className="titulo">Olá Mundo! &nbsp;{<VolunteerActivismIcon/>}</h1>
       <h2 className="subtitulo">Este é meu primeiro site usando React</h2>
 
-      {/* <Luana nome = "luana"/>
-      <Luana nome = "churros"/>
-      <ul>
-        {listaProdutos.map((produto) =>{
-          return (
-            <Churros preco={produto.preco} nome={produto.nome}/>
-          )
-        })}
-      </ul> */}
-
       <p className="numeracao">{contador}</p>
       <div className='botoes'>
         <Button onClick={aumentar} variant="contained" color="secondary"  size="small">Aumentar</Button>
         <Button onClick={diminuir} variant="contained" color="secondary"  size="small">Diminuir</Button>
+
       </div>
+      <div>
+          <img alt="leitura" className='imagem__leitura' src={imagemEstudos}/>
+        </div>
       <footer className="rodape">
         <a href="https://github.com/luanafront/primeiro-react" class="link__imagem">
           <img alt="github" className='imagem__github' src={imagemGithub}/>
